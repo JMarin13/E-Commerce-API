43a899fae2c46f5cc51e1967e36efb90cebf6ddf
JMarin13 <jmarintobar@gmail.com>
2025-09-04 15:35:23 -0500

    Cambios generales

---
+++ E-Commerce-API/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_API.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
